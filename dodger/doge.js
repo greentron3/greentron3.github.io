a1d204a0c56c16f4052bb613e87cffc13e3a82cf
Behruze <behruze@skilstak.sh>
2016-10-26 17:17:22 -0400

    save

--- dodger/doge.js
+++ dodger/doge.js
@@ -1,1 +1,15 @@
+class Boot {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+create()
+}
+
 var game = new Phaser.Game(320,568);
+game.state.add("Boot",Boot);
+game.state.start("Boot");
+
+
+
