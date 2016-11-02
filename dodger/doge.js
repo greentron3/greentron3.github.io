ce6bca35227825cc41006ef5d74dd0286b41dbe5
Behruze <behruze@skilstak.sh>
2016-11-02 17:18:58 -0400

    save

--- dodger/doge.js
+++ dodger/doge.js
@@ -4,9 +4,30 @@ class Boot {
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
   }
-create()
+create() {
+   this.state.start("Load")
+}
 }
 
+class Load {
+    preload() {
+      console.log("Loading...");
+      this.load.image("bg","assets/irontrump.png")
+    }
+    create() {
+      console.log("Loaded");
+      this.state.start("Play")
+    } 
+
+} 
+                   
+   class Play {
+     create() {
+    console.log("Entered Play ");
+     }
+            
+   }                     
+                      
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",Boot);
 game.state.start("Boot");
